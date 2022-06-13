489ca9864f3c858c232036c4cbf8e64a69fc0930
omarf <cc201010@fhstp.ac.at>
2022-06-13 23:45:04 +0200

    Movment,Jump

---
+++ Assets/Scripts/CameraFollows.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollows : MonoBehaviour
+{
+    // Global Variables 
+    
+    // The target object = Player
+    public Transform targetObject;
+    
+    // Default distance between the cam and the player
+    public Vector3 camerOffset;
+    
+    // Smooth factor for camera rotation : Smooth the camera movement 
+    public float smoothFactor = 0.5f;
+    
+    // Check weather the camera looked at the player or not 
+    public bool lookAtTarget = true;
+    
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // calculate the distane between the cam and the player
+        camerOffset = transform.position - targetObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // Calculate the new position 
+        Vector3 newPosition = targetObject.transform.position + camerOffset;
+        
+        // Set the position of the camera to the new position
+        transform.position = Vector3.Slerp(transform.position,newPosition,smoothFactor);
+        
+        // Camera rotation change 
+        if (lookAtTarget)
+        {
+            transform.LookAt(targetObject);
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerMovment.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+    // Global Variables 
+    
+    // Controlling the player speed
+    [SerializeField] private float speed = 5.0f;
+    
+    // Forward & Backward movement 
+    private float forwardInput;
+    
+    // Left & Right movement 
+    private float horizontalInput;
+    
+    // Rotation speed
+    [SerializeField] private float rotationSpeed = 270.0f;
+    
+    // How high the player can jump
+    [SerializeField] private float jumpForce = 5.0f;
+    
+    // Declare the rigid body 
+    private Rigidbody playerRb;
+    
+    // Check if the player is on the ground 
+    public bool isOnGround = true;
+    
+    // Camera follow the player
+    //public Camera followCamera;
+
+    // Camera Position
+    //private Vector3 m_CameraPos;
+    
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Access the rigid body 
+        playerRb = GetComponent<Rigidbody>();
+        
+        // Get camera position 
+        //m_CameraPos = followCamera.transform.position - transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Camera following the player 
+       // followCamera.transform.position = playerRb.position + m_CameraPos;
+        
+        // Get player input
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        Vector3 movmentDirection = new Vector3(horizontalInput, 0, forwardInput);
+        movmentDirection.Normalize();
+        
+        // Move the player forward 
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+        
+        // change the position based on the movement direction 
+        transform.Translate(movmentDirection * speed * Time.deltaTime,Space.World);
+        
+        // Check if the player is moving 
+        if (movmentDirection != Vector3.zero)
+        {
+            // if its moving we want the player to rotate to face the direction its moving
+            // Store rotations
+            Quaternion toRotation = Quaternion.LookRotation(movmentDirection,Vector3.up);
+            
+            // Change the rotation of the player
+            transform.rotation = Quaternion.RotateTowards(transform.rotation,toRotation,rotationSpeed * Time.deltaTime);
+            
+        }
+        
+        // Let the player jump
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce,ForceMode.Impulse);
+            isOnGround = false;
+        }
+        
+       
+    }
+    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+    }
+}
