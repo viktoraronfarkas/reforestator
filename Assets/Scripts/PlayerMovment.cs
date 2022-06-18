1abfc4587f97d096a0d084d727a3c6090484adad
Viktor Farkas <viktoraron.farkas@gmail.com>
2022-06-18 12:38:34 +0200

    add idle animation

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -4,43 +4,47 @@ using UnityEngine;
 
 public class PlayerMovment : MonoBehaviour
 {
-    // Global Variables 
-    
+    // Global Variables
+
     // Controlling the player speed
     [SerializeField] private float speed = 5.0f;
-    
-    // Forward & Backward movement 
+
+    // Forward & Backward movement
     private float forwardInput;
-    
-    // Left & Right movement 
+
+    // Left & Right movement
     private float horizontalInput;
-    
+
     // Rotation speed
     [SerializeField] private float rotationSpeed = 270.0f;
-    
+
     // How high the player can jump
     [SerializeField] private float jumpForce = 5.0f;
-    
-    // Declare the rigid body 
+
+    // Declare the rigid body
     private Rigidbody playerRb;
-    
-    // Check if the player is on the ground 
+
+    // Check if the player is on the ground
     public bool isOnGround = true;
-    
-    
+
+    public bool isWalking = false;
+
+    public Animator animator;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        // Access the rigid body 
+        // Access the rigid body
         playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Camera following the player 
+        // Camera following the player
        // followCamera.transform.position = playerRb.position + m_CameraPos;
-        
+
         // Get player input
         forwardInput = Input.GetAxis("Vertical");
         horizontalInput = Input.GetAxis("Horizontal");
@@ -49,21 +53,24 @@ public class PlayerMovment : MonoBehaviour
         Vector3 movmentDirection = new Vector3(horizontalInput, 0, forwardInput);
         movmentDirection.Normalize();
 
-        // change the position based on the movement direction 
+        // change the position based on the movement direction
         transform.Translate(movmentDirection * speed * Time.deltaTime,Space.World);
-        
-        // Check if the player is moving 
+
+        // Check if the player is moving
         if (movmentDirection != Vector3.zero)
         {
             // if its moving we want the player to rotate to face the direction its moving
             // Store rotations
             Quaternion toRotation = Quaternion.LookRotation(movmentDirection,Vector3.up);
-            
+
             // Change the rotation of the player
             transform.rotation = Quaternion.RotateTowards(transform.rotation,toRotation,rotationSpeed * Time.deltaTime);
-            
+
         }
-        
+
+        // Animations
+        animator.SetBool("isWalking", Input.GetAxisRaw("Vertical") != 0);
+
         // Let the player jump
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
